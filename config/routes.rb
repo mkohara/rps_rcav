00ecdbc6ed20fc680d915462ac85a90ffb3fad80
Maggie O'Hara <mkohara@users.noreply.github.com>
2017-01-28 14:40:54 -0600

    I think I figured out the home url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  get("/", { :controller=> "games", :action=> "home"})
   get("/rock", { :controller => "games", :action => "play_rock" })
 
   get("/paper", { :controller => "games", :action => "play_paper" })
